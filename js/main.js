b5f2efb3dada5afd7f9f23b259c0d55aeb62561b
mahmoudsobhy1912 <mahmoud_20180563@fci.helwan.edu.eg>
2021-02-03 04:28:08 +0200

    updat navbar

--- js/main.js
+++ js/main.js
@@ -1,25 +1,38 @@
 const elements = {
-    nav: document.querySelector(".header"),
-    ul: document.querySelector("ul"),
+    header: document.querySelector(".header"),
+    nav: document.querySelector("nav"),
+    ul: document.querySelector(".navbar nav ul"),
     one: document.querySelector(".sec-one"),
     img: document.querySelector(".all-img"),
     footer: document.querySelector("footer"),
-    icon: document.querySelector("#nav-icon"),
-    close: document.querySelector("#nav-close"),
+    icon: document.querySelector(".nav-icon"),
+    close: document.querySelector(".nav-close"),
     mass: document.querySelector(".message")
 }
 
 
 elements.icon.addEventListener("click", () => {
-    
     elements.one.classList.add("hidden") 
     elements.img.classList.add("hidden") 
     elements.footer.classList.add("hidden") 
     elements.icon.classList.add("hidden") 
     elements.mass.classList.add("hidden") 
-    elements.nav.classList.add("nav-m") 
-    elements.nav.classList.add("list") 
-    elements.icon.style.display = "none";
-    elements.close.style.display = "initial";
-    elements.ul.style.display = "initial";
+    elements.header.classList.add("nav-m") 
+    elements.nav.classList.add("show") 
+    elements.ul.classList.add("list") 
+    elements.close.classList.add("show") 
+})
+
+elements.close.addEventListener("click", () => {
+    elements.one.classList.remove("hidden") 
+    elements.img.classList.remove("hidden") 
+    elements.footer.classList.remove("hidden") 
+    elements.icon.classList.remove("hidden") 
+    elements.mass.classList.remove("hidden") 
+    elements.header.classList.remove("nav-m") 
+    elements.nav.classList.remove("show") 
+    elements.ul.classList.remove("list") 
+    elements.close.classList.remove("show") 
+    elements.icon.classList.remove("hidden") 
+
 })
